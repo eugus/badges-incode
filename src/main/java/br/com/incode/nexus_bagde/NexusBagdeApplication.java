a2e3e64acafaea519d78ba247b117e5c64b0de34
luis.lima <luis.lima@ceuma.br>
2025-07-31 10:34:38 -0300

    subindo configs iniciais

---
+++ src/main/java/br/com/incode/nexus_bagde/NexusBagdeApplication.java
@@ -0,0 +1,13 @@
+package br.com.incode.nexus_bagde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusBagdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusBagdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/incode/nexus_bagde/NexusBagdeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.incode.nexus_bagde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusBagdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
