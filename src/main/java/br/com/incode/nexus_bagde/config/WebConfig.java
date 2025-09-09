0eea6ecdfdddbc508832a985a53926c0f665c13e
luis.lima <luis.lima@ceuma.br>
2025-09-09 15:30:10 -0300

    ajustando webConfig

--- src/main/java/br/com/incode/nexus_bagde/config/WebConfig.java
+++ src/main/java/br/com/incode/nexus_bagde/config/WebConfig.java
@@ -15,7 +15,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
+                //.allowedOrigins("http://localhost:3000")
                 .allowedOrigins("https://badge-gerador.vercel.app")
+
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
